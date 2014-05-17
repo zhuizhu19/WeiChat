5a7c9f9a733fa8ece1c060d3c27ac628a8035af4
qixiaobo <qixiaobo.nj@gmail.com>
2014-05-17 21:54:19 +0800

    aunties error

--- src/main/java/org/liyou/qixiaobo/daos/AuntiesDao.java
+++ src/main/java/org/liyou/qixiaobo/daos/AuntiesDao.java
@@ -3,11 +3,13 @@ package org.liyou.qixiaobo.daos;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.liyou.qixiaobo.entities.hibernate.Aunties;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -22,9 +24,12 @@ public class AuntiesDao extends BaseDao<Aunties> {
         Session session = sessionFacotry.getCurrentSession();
         Transaction tx = session.beginTransaction();
         Criteria criteria = session.createCriteria(Aunties.class);
-        criteria.add(Restrictions.le("auntDate", date));
+        criteria.add(Restrictions.le("auntDate", date)).addOrder(Order.desc("auntDate"));
         criteria.setMaxResults(nums);
         List<Aunties> aunties = criteria.list();
+        if(aunties!=null&&aunties.size()>0){
+            Collections.reverse(aunties);
+        }
         tx.commit();
         return aunties;
     }
